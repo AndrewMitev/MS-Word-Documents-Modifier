64ade5f03f558c7f789d17f18973809b55b4a25f
Andrey Mitev <amitev@savvysherpa.com>
2022-02-09 15:37:39 +0200

    Initial

---
+++ DictionaryPrepare/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows;
+
+using WpfColorFontDialog;
+using DocumentServices;
+
+
+namespace DictionaryPrepare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool _isAddingHeaders = true;
+        private bool _isNumberedDictionary = false;
+        private bool _isSpecificFontSelect = false;
+
+        private string _fontFamily;
+        private double _fontSize;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddHeader.IsChecked = true;
+            NormalDictionary.IsChecked = true;
+        }
+
+        private void Drag_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data != null && e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                FileNameDisplay.Content = files[0];
+                FileNameDisplay.Visibility = Visibility.Visible;
+
+                GenerateButton.IsEnabled = true;
+            }
+        }
+
+        private void AddHeader_Toggle(object sender, RoutedEventArgs e)
+        {
+            RemoveHeader.IsChecked = false;
+            _isAddingHeaders = true;
+        }
+
+        private void RemoveHeader_Toggle(object sender, RoutedEventArgs e)
+        {
+            AddHeader.IsChecked = false;
+            _isAddingHeaders = false;
+        }
+
+        private void NormalDictionary_Toggle(object sender, RoutedEventArgs e)
+        {
+            NumberedDictionary.IsChecked = false;
+            _isNumberedDictionary = false;
+        }
+
+        private void NumberedDictionary_Toggle(object sender, RoutedEventArgs e)
+        {
+            NormalDictionary.IsChecked = false;
+            _isNumberedDictionary = true;
+        }
+
+        private void Generate_Handler(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_isAddingHeaders)
+                {
+                    if (!_isSpecificFontSelect)
+                    {
+                        Document.AddHeadings(FileNameDisplay.Content.ToString(), _isNumberedDictionary);
+                    }
+                    else 
+                    {
+                        Document.AddHeadings(FileNameDisplay.Content.ToString(), _isNumberedDictionary, _fontFamily, _fontSize);
+                    }
+                }
+                else
+                {
+                    Document.RemoveHeadings(FileNameDisplay.Content.ToString());
+                }
+
+                FileNameDisplay.Content = string.Empty;
+                FileNameDisplay.Visibility = Visibility.Hidden;
+
+                GenerateButton.IsEnabled = false;
+
+                MessageBox.Show("Success!");
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FontHandler(object sender, EventArgs args)
+        {
+            ColorFontDialog dialog = new ColorFontDialog(true, false);
+            dialog.Font = FontInfo.GetControlFont(MyTextBox);
+
+            dialog.ShowDialog();
+
+            FontInfo font = dialog.Font;
+
+            if (font != null)
+            {
+                _fontFamily = font.Family.Source;
+                _fontSize = font.Size;
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            FontPicker.IsEnabled = true;
+            _isSpecificFontSelect = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            FontPicker.IsEnabled = false;
+            _isSpecificFontSelect = false;
+        }
+    }
+}
---
+++ DocumentServices/Document.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+namespace DocumentServices
+{
+    public static class Document
+    {
+        public static void AddHeadings(string filePath, bool isNumberedDictionary, string fontFamily = "", double fontSize = 0.0d)
+        {
+            using (var document = DocX.Load(filePath))
+            {
+                var paragraphs = document.Paragraphs;
+                StringBuilder lemmaBuilder = new StringBuilder();
+
+                foreach (var paragraph in paragraphs)
+                {
+                    foreach (var word in paragraph.MagicText)
+                    {
+                        bool isDefaultLemma = word.text != string.Empty && word.formatting.Bold == true;
+
+                        bool isSpecifiedLemma = fontFamily != string.Empty || fontSize != 0.0d;
+
+                        bool isSpecificationMatched = word.formatting.FontFamily.Name.ToLower().Equals(fontFamily.ToLower()) 
+                            && word.formatting.Size == fontSize;
+
+                        if (isDefaultLemma && (isSpecifiedLemma ? isSpecificationMatched : true))
+                        {
+                            int currentIndex = paragraph.MagicText.IndexOf(word);
+                            string paragraphText = isAdditionalAssemblyDone(paragraph.MagicText, lemmaBuilder, ++currentIndex) ?
+                                word.text + lemmaBuilder.ToString() :
+                                word.text;
+
+                            paragraphText = (paragraphText.EndsWith(".") || paragraphText.EndsWith(",")) ?
+                                paragraphText.Substring(0, paragraphText.Length - 1) : paragraphText;
+
+                            paragraphText = isNumberedDictionary ?
+                                paragraphText :
+                                PrePendText(paragraph.MagicText, paragraph.MagicText.IndexOf(word)) + paragraphText;
+
+                            var p = paragraph.InsertParagraphBeforeSelf(paragraphText);
+                            p.StyleId = HeadingType.Heading1.ToString();
+                            p.FontSize(16.0);
+                            p.Bold(false);
+                            p.SpacingBefore(12.0d);
+
+                            lemmaBuilder.Clear();
+                            break;
+                        }
+                    }
+                }
+
+                document.SaveAs(GetOuputFilename(filePath));
+            }
+        }
+
+        public static void RemoveHeadings(string filePath) 
+        {
+            using (var document = DocX.Load(filePath))
+            {
+                foreach (var pars2 in document.Paragraphs)
+                {
+                    if (pars2.StyleId == HeadingType.Heading1.ToString() || pars2.StyleId == "Kop1")
+                    {
+                        document.RemoveParagraph(pars2);
+                    }
+                }
+
+                document.SaveAs(GetOuputFilename(filePath));
+            }
+        }
+
+        private static string GetOuputFilename(string originalFilePath)
+        {
+            return originalFilePath.Insert(originalFilePath.LastIndexOf('.'), "_output");
+        }
+
+        private static string PrePendText(List<FormattedText> paragraph, int currentIndex)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < currentIndex; i++)
+            {
+                sb.Append(paragraph[i].text);
+            }
+
+            return sb.ToString();
+        }
+
+        private static bool isAdditionalAssemblyDone(List<FormattedText> paragraph, StringBuilder lemmaBuilder, int index)
+        {
+            FormattedText word = paragraph[index];
+
+            if (word.text != string.Empty && word.text != "," && word.text != ", ")
+            {
+                if (word.formatting.Bold == true)
+                {
+                    lemmaBuilder.Append(word.text);
+                    return true;
+                }
+
+                return false;
+            }
+
+            lemmaBuilder.Append(word.text);
+            return isAdditionalAssemblyDone(paragraph, lemmaBuilder, ++index);
+        }
+    }
+}
